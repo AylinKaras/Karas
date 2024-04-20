66eae0fd843dd6f4929a41d0d38b52e1f1e96620
AylinKaras <161029380+AylinKaras@users.noreply.github.com>
2024-04-20 16:33:57 +0500

    karas

---
+++ main.js
@@ -0,0 +1,38 @@
+// let menu = document.querySelector(".fixed-menu");
+
+// menu.addEventListener('click', function (event) {
+//     let clickTarget = event.target;
+//     let activeBtn = document.querySelector('.active');
+
+//     if (clickTarget.classList.contains('nav-link')) {
+//         clickTarget.classList.add('active');
+//         activeBtn.classList.remove('active');
+//     }
+// });
+// document.querySelector(".mobile-button").addEventListener("click", function (event) {
+//     document, querySelector(".mobile-menu").classList.toggle("hide");
+// });
+// menu.addEventListener('click', function (event) {
+//     let clickTarget = event.target;
+//     let activeBtn = document.querySelector('.active');
+//     if (clickTarget.classList.contains('nav-link')) {
+//         clickTarget.classList.add('active');
+//         activeBtn.classList.remove('active');
+
+//         let mobile = document.querySelector('.mobile-menu');
+//         if (!mobile.classList.contains('hide')) {
+//             mobile.classList.add('hide');
+//         }
+
+//     }
+// });
+// let body = document.querySelector("body");
+// let mobileMenu = document.querySelector(".mobile-menu");
+// if (!body.classList.contains("off-scroll") &&
+//     !mobileMenu.classList.contains('hide')) {
+//     body.classList.add("of-scroll");
+// }
+// else {
+//     body.classList.remove("off-scroll");
+// }
+// });
